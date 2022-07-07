427fbae3b6e2aff1cfea5ae4e1c99aa3ab698d18
Kisalay <kisalaykisu@gmail.com>
2022-07-07 18:52:45 +0530

    first commit

---
+++ src/com/bridgelabz/UserRegistration_WithoutRegex.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class UserRegistration_WithoutRegex {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the UserRegistration");
+    }
+}
